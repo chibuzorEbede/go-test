fdefc789e54eeba5367f4540b5f4bad8761cf5fd
Chibuzor Ebede <chibuzorebede@gmail.com>
2024-11-17 08:37:27 -0500

    trying out golang

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import "fmt"
 
-func add(x int, y int) int {
+func add(x, y int) int {
 	return x + y
+}
+
+func swap(x string, y string) (string,string){
+	return y, x
 }
 
 func main() {
 	
-	fmt.Println(add(13,2))
+	fmt.Println(add(19,2))
+	a,b := swap("hi","mom")
+	fmt.Println(a,b)
 }
